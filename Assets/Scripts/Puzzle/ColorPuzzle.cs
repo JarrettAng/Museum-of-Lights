45634e321231b915621d2e1c2c400f4ad755638c
JarrettAng <jarrett_ang@hotmail.com>
2024-12-26 22:52:52 +0800

    Fonts + Room 1 done

--- Assets/Scripts/Puzzle/ColorPuzzle.cs
+++ Assets/Scripts/Puzzle/ColorPuzzle.cs
@@ -1,9 +1,10 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 [System.Serializable]
 public struct ColorSolution {
-    ColorSpot r_spot;
-    int m_correctColor;
+    public ColorSpot r_Spot;
+    public int m_CorrectColor;
 }
 
 public class ColorPuzzle : Puzzle
@@ -11,22 +12,25 @@ public class ColorPuzzle : Puzzle
     [SerializeField]
     private ColorSolution[] m_colorSolutions;
 
+    private bool m_correctColors = false;
+
     new void Update() {
         base.Update();
 
-        HandleInput();
-        CheckStatus();
+        if (m_solvedOnce && m_correctColors) return;
 
-        void HandleInput() {
-           
-        }
-
-        void CheckStatus() {
-            
+        if (m_LockedIn) {
+            m_correctColors = true;
+            for (int i = 0; i < m_colorSolutions.Length; i++) {
+                if (m_colorSolutions[i].r_Spot.m_currentColor != m_colorSolutions[i].m_CorrectColor) {
+                    m_correctColors = false;
+                    break;
+                }
+            }
         }
     }
 
     protected override bool CheckSolved() {
-        return true;
+        return m_correctColors;
     }
 }
